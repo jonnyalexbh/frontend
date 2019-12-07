8acb66e4d2c3c74d2b8fc3fa98c74dabbf38e3e8
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-12-06 22:45:34 -0500

    string interpolation

--- store-with-angular8p3/src/app/app.component.ts
+++ store-with-angular8p3/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store-with-angular8p3';
+  title = 'store with angular 8p3';
 }
