a3dd613b199ea6b56c9738ac9aefc68bc2fcb38b
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-12-07 06:55:19 -0500

    use of ngFor to navigate objects

--- store-with-angular8p3/src/app/app.component.ts
+++ store-with-angular8p3/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'store with angular 8p3';
   items = ['Tankis', 'Jose Miguel', 'Leonardo'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
 
   addItem() {
     this.items.push('new item');
