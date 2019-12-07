206e60854803a521474b74210194787516be772e
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-12-07 08:16:35 -0500

    component life cycle

--- store-with-angular8p3/src/app/components/product.component.ts
+++ store-with-angular8p3/src/app/components/product.component.ts
@@ -1,13 +1,43 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
-import { Product } from '../product.model'
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1 - this is the builder');
+  }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('2 - ngOnChanges', changes);
+  // }
+
+  ngOnInit() {
+    console.log('3 - this is the ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('4 - this is the ngDoCheck');
+  }
+
+  ngOnDestroy() {
+    console.log('5 - this is the ngOnDestroy');
+  }
 
   addCart() {
     console.log('Se agrego un producto al carrito');
