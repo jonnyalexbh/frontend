b2e0b18338bfe8a0d80802f844c7c3296d565dd2
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-12-07 10:54:16 -0500

    creating angular paths

--- store-with-angular8p3/src/app/app.component.ts
+++ store-with-angular8p3/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,65 +6,5 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = '';
-  items = ['Tankis', 'Jose Miguel', 'Leonardo'];
 
-  power = 10;
-
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-  ];
-
-  addItem() {
-    this.items.push('new item');
-  }
-
-  deleteItem(index: number) {
-    this.items.splice(index, 1);
-  }
-
-  clickProduct(id: number) {
-    console.log(`printing from parent component ${id}`);
-  }
 }
