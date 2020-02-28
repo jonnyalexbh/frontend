ef3b40c363b857f59b0a0ad4df08169c3a8a2876
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-02-28 15:58:33 -0500

    unit test title app component

--- testing-with-angular/src/app/app.component.ts
+++ testing-with-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testing-with-angular';
+  title = 'testing with angular';
 }
