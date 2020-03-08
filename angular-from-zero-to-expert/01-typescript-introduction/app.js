92ae759d2680e3f51c57e46dd457acdf90fa5e44
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-03-08 12:18:39 -0500

    typescript demo

---
+++ angular-from-zero-to-expert/01-typescript-introduction/app.js
@@ -0,0 +1,9 @@
+(function () {
+    function greet(name) {
+        console.table('Hello ' + name); // Hola Logan
+    }
+    var wolverine = {
+        name: 'Logan'
+    };
+    greet(wolverine.name);
+})();
---
+++ angular-from-zero-to-expert/01-typescript-introduction/app.ts
@@ -0,0 +1,13 @@
+(function () {
+    function greet(name: string) {
+        console.table('Hello ' + name); // Hola Logan
+    }
+
+    const wolverine = {
+        name: 'Logan'
+    };
+
+    greet(wolverine.name);
+
+})();
+
