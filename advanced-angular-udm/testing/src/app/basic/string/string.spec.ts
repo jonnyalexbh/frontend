ebdf95d8107409f8a89faf568d88ecfab2d0ee38
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-03-14 12:29:58 -0500

    check that the result contains something

--- advanced-angular-udm/testing/src/app/basic/string/string.spec.ts
+++ advanced-angular-udm/testing/src/app/basic/string/string.spec.ts
@@ -1,8 +1,16 @@
 import { message } from './string';
 
 describe('string tests', () => {
+
   it('should return a string', () => {
     const resp = message('jonnyalexbh');
     expect(typeof resp).toBe('string');
   });
+
+  it('should return a greeting with the name sent', () => {
+    const name = 'Tankis'
+    const resp = message(name);
+    expect(resp).toContain(name);
+  });
+
 });
