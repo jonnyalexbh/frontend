05179bcd45881b63c472b03e6f079d91120fe92f
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-03-17 10:36:52 -0500

    observer and subscriber

--- reactivex-rxjs/rxjs/src/index.ts
+++ reactivex-rxjs/rxjs/src/index.ts
@@ -1,4 +1,10 @@
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<any> = {
+  next: value => console.log('[next]:', value),
+  error: error => console.warn('[error]:', error),
+  complete: () => console.info('completed [obs]')
+};
 
 // const obs$ = Observable.create();
 const obs$ = new Observable<string>(subs => {
@@ -8,10 +14,20 @@ const obs$ = new Observable<string>(subs => {
   subs.next('Hello');
   subs.next('Tankis');
 
+  // force an error
+  // const a = undefined;
+  // a.name = 'Jonny';
+
   subs.complete();
 
   subs.next('Hello');
   subs.next('Jose');
 });
 
-obs$.subscribe(console.log);
+obs$.subscribe(observer);
+
+// obs$.subscribe(
+//   valor => console.log('next', valor),
+//   error => console.warn('error:', error),
+//   () => console.info('completed')
+// );
