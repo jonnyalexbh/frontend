e63a89f2b9976c080b456802949f8255dbbec796
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-03-22 11:06:09 -0500

    operator time debounce-time

---
+++ reactivex-rxjs/rxjs/src/operators-time/01-debounceTime.ts
@@ -0,0 +1,23 @@
+import { fromEvent, pipe } from "rxjs";
+import { debounceTime, pluck, distinctUntilChanged } from "rxjs/operators";
+
+const click$ = fromEvent(document, 'click');
+
+click$
+  .pipe(debounceTime(3000))
+  .subscribe(console.log);
+
+// example 2
+
+const input = document.createElement('input');
+document.querySelector('body').append(input);
+
+const input$ = fromEvent(input, 'keyup');
+
+input$
+  .pipe(
+    debounceTime(1000),
+    pluck('target', 'value'),
+    distinctUntilChanged()
+  )
+  .subscribe(console.log);
