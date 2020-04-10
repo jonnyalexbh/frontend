11369d47de14cc541c706720e2679da45f60a00e
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-04-10 17:04:36 -0500

    introduction to data types

---
+++ angular-from-zero-to-expert/01-typescript-introduction/types.ts
@@ -0,0 +1,22 @@
+(function () {
+
+  let mesagge: string = 'Hello';
+  let number: number = 123;
+  let boolean: boolean = false; // false
+  let today: Date = new Date();
+
+  let anything: string | number | boolean | Date;
+  anything = mesagge;
+  anything = number;
+  anything = boolean;
+  anything = today;
+
+  console.log(today);
+
+  let spiderman = {
+    name: 'jonnyalex.bh',
+    age: 29,
+  }
+
+})();
+
