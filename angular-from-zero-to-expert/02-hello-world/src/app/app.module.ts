9b0a502dcff4f4272b5ba6e31fdef84760bac659
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-04-14 18:07:36 -0500

    creating the footer.component

--- angular-from-zero-to-expert/02-hello-world/src/app/app.module.ts
+++ angular-from-zero-to-expert/02-hello-world/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './components/body/body.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
