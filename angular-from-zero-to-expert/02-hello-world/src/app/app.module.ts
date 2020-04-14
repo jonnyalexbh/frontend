6f4a790eca65473aa06aa7df9c109c1088730d31
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-04-14 14:47:05 -0500

    template url: separating the HTML from the component

--- angular-from-zero-to-expert/02-hello-world/src/app/app.module.ts
+++ angular-from-zero-to-expert/02-hello-world/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { HeaderComponent } from './components/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule
