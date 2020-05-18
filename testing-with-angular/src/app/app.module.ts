738bab91158fb2f33a28f3b22627423c87cd6fc1
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-05-18 16:12:48 -0500

    automatically generated spec files using angular cli

--- testing-with-angular/src/app/app.module.ts
+++ testing-with-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleUserComponent } from './screens/sample-user/sample-user.component';
 import { DoctorsComponent } from './screens/basic-unit-tests/doctors/doctors.component';
+import { HospitalComponent } from './screens/integration-testing/hospital/hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SampleUserComponent,
-    DoctorsComponent
+    DoctorsComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule,
